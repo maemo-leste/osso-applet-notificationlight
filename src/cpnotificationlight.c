800745fbd60861c91f1111805844e2aa6b3c5b65
uvos <devnull@uvos.xyz>
2022-04-01 18:30:27 +0200

    migrate away from gconf

--- src/cpnotificationlight.c
+++ src/cpnotificationlight.c
@@ -22,21 +22,100 @@
 #include <libintl.h>
 #include <string.h>
 #include <locale.h>
-#include <gconf/gconf-client.h>
 #include <gdk/gdkx.h>
+#include <mce/dbus-names.h>
 
-#define GCONF_KEY_LED_NOTIFICATION    "/system/osso/dsm/leds/"
-#define GCONF_KEY_DEVICE_ON           GCONF_KEY_LED_NOTIFICATION "PatternDeviceOn"
-#define GCONF_KEY_COMMUNICATION_CALL  GCONF_KEY_LED_NOTIFICATION "PatternCommunicationCall"
-#define GCONF_KEY_COMMUNICATION_SMS   GCONF_KEY_LED_NOTIFICATION "PatternCommunicationSMS"
-#define GCONF_KEY_COMMUNICATION_EMAIL GCONF_KEY_LED_NOTIFICATION "PatternCommunicationEmail"
-#define GCONF_KEY_COMMUNICATION_IM    GCONF_KEY_LED_NOTIFICATION "PatternCommunicationIM"
-#define GCONF_KEY_BATTERY_CHARGING    GCONF_KEY_LED_NOTIFICATION "PatternBatteryCharging"
-#define GCONF_KEY_COMMON_NOTIFICATION GCONF_KEY_LED_NOTIFICATION "PatternCommonNotification"
+#define KEY_DEVICE_ON           "PatternDeviceOn"
+#define KEY_COMMUNICATION_CALL  "PatternCommunicationCall"
+#define KEY_COMMUNICATION_SMS   "PatternCommunicationSMS"
+#define KEY_COMMUNICATION_EMAIL "PatternCommunicationEmail"
+#define KEY_COMMUNICATION_IM    "PatternCommunicationIM"
+#define KEY_BATTERY_CHARGING    "PatternBatteryCharging"
+#define KEY_COMMON_NOTIFICATION "PatternCommonNotification"
 
-static void
-_size_changed(GdkScreen* screen,
-              gpointer user_data)
+
+static GDBusConnection *get_dbus_connection(void)
+{
+	GError *error = NULL;
+	char *addr;
+	
+	GDBusConnection *s_bus_conn;
+
+	addr = g_dbus_address_get_for_bus_sync(G_BUS_TYPE_SYSTEM, NULL, &error);
+	if (addr == NULL) {
+		g_error("fail to get dbus addr: %s\n", error->message);
+		g_free(error);
+		return NULL;
+	}
+
+	s_bus_conn = g_dbus_connection_new_for_address_sync(addr,
+			G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT |
+			G_DBUS_CONNECTION_FLAGS_MESSAGE_BUS_CONNECTION,
+			NULL, NULL, &error);
+
+	if (s_bus_conn == NULL) {
+		g_error("fail to create dbus connection: %s\n", error->message);
+		g_free(error);
+	}
+
+	return s_bus_conn;
+}
+
+static gboolean mce_get_pattern_bool(GDBusConnection *bus, const gchar *key, gboolean defval)
+{
+	GVariant *result;
+
+	GError *error = NULL;
+
+	result = g_dbus_connection_call_sync(bus, MCE_SERVICE, MCE_REQUEST_PATH,
+		MCE_REQUEST_IF, MCE_LED_PATTERN_DISABLED, g_variant_new("(s)", key), NULL,
+		G_DBUS_CALL_FLAGS_NONE, 2000, NULL, &error);
+	
+	if (error || !result) {
+		g_critical("%s: Can not get value for %s", __func__, MCE_LED_PATTERN_DISABLED);
+		return defval;
+	}
+
+	GVariantType *int_variant = g_variant_type_new("(b)");
+
+	if (!g_variant_is_of_type(result, int_variant)) {
+		g_critical("%s: Can not get value for %s wrong type: %s instead of (b)",
+				   __func__, MCE_LED_PATTERN_DISABLED, g_variant_get_type_string(result));
+		g_variant_unref(result);
+		g_variant_type_free(int_variant);
+		return defval;
+	}
+
+	g_variant_type_free(int_variant);
+
+	gboolean value;
+	g_variant_get(result, "(b)", &value);
+	g_variant_unref(result);
+	return value;
+}
+
+static gboolean mce_set_pattern_bool(GDBusConnection *bus, const gchar *key, gboolean val)
+{
+	GVariant *result;
+	GError *error = NULL;
+	
+	const gchar *request = val ? MCE_ENABLE_LED_PATTERN : MCE_DISABLE_LED_PATTERN;
+
+	result = g_dbus_connection_call_sync(bus, MCE_SERVICE, MCE_REQUEST_PATH,
+		MCE_REQUEST_IF, request, g_variant_new("(s)", key), NULL,
+		G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+
+	if (error) {
+		g_critical("%s: Failed to send %s to mce", __func__, request);
+		return FALSE;
+	}
+
+	g_variant_unref(result);
+
+	return TRUE;
+}
+
+static void _size_changed(GdkScreen* screen, gpointer user_data)
 {
 	GdkGeometry geometry;
 	if(gdk_screen_get_width(gdk_display_get_default_screen(gdk_display_get_default())) < 800) {
@@ -57,8 +136,9 @@ osso_return_t execute(osso_context_t* osso, gpointer user_data, gboolean user_ac
 	                    GTK_DIALOG_MODAL | GTK_DIALOG_NO_SEPARATOR,
 	                    dgettext("hildon-libs", "wdgt_bd_save"), GTK_RESPONSE_ACCEPT,
 	                    NULL);
-	GConfClient* gconf_client = gconf_client_get_default();
-	g_assert(GCONF_IS_CLIENT(gconf_client));
+
+	GDBusConnection *s_bus = get_dbus_connection();
+	g_assert(s_bus);
 
 	GtkWidget* pan = hildon_pannable_area_new();
 	GtkWidget* box = gtk_vbox_new(TRUE, 0);
@@ -82,19 +162,19 @@ osso_return_t execute(osso_context_t* osso, gpointer user_data, gboolean user_ac
 	                     "disp_li_led_other_notifications"));
 
 	hildon_check_button_set_active(HILDON_CHECK_BUTTON(device_on_check_button),
-	                               gconf_client_get_bool(gconf_client, GCONF_KEY_DEVICE_ON, NULL));
+	                               mce_get_pattern_bool(s_bus, KEY_DEVICE_ON, FALSE));
 	hildon_check_button_set_active(HILDON_CHECK_BUTTON(missed_call_check_button),
-	                               gconf_client_get_bool(gconf_client, GCONF_KEY_COMMUNICATION_CALL, NULL));
+	                               mce_get_pattern_bool(s_bus, KEY_COMMUNICATION_CALL, FALSE));
 	hildon_check_button_set_active(HILDON_CHECK_BUTTON(text_msg_check_button),
-	                               gconf_client_get_bool(gconf_client, GCONF_KEY_COMMUNICATION_SMS, NULL));
+	                               mce_get_pattern_bool(s_bus, KEY_COMMUNICATION_SMS, FALSE));
 	hildon_check_button_set_active(HILDON_CHECK_BUTTON(email_msg_check_button),
-	                               gconf_client_get_bool(gconf_client, GCONF_KEY_COMMUNICATION_EMAIL, NULL));
+	                               mce_get_pattern_bool(s_bus, KEY_COMMUNICATION_EMAIL, FALSE));
 	hildon_check_button_set_active(HILDON_CHECK_BUTTON(im_msg_check_button),
-	                               gconf_client_get_bool(gconf_client, GCONF_KEY_COMMUNICATION_IM, NULL));
+	                               mce_get_pattern_bool(s_bus, KEY_COMMUNICATION_IM, FALSE));
 	hildon_check_button_set_active(HILDON_CHECK_BUTTON(charging_check_button),
-	                               gconf_client_get_bool(gconf_client, GCONF_KEY_BATTERY_CHARGING, NULL));
+	                               mce_get_pattern_bool(s_bus, KEY_BATTERY_CHARGING, FALSE));
 	hildon_check_button_set_active(HILDON_CHECK_BUTTON(other_check_button),
-	                               gconf_client_get_bool(gconf_client, GCONF_KEY_COMMON_NOTIFICATION, NULL));
+	                               mce_get_pattern_bool(s_bus, KEY_COMMON_NOTIFICATION, FALSE));
 
 	gtk_box_pack_start(GTK_BOX(box), device_on_check_button, TRUE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(box), missed_call_check_button, TRUE, FALSE, 0);
@@ -123,37 +203,30 @@ osso_return_t execute(osso_context_t* osso, gpointer user_data, gboolean user_ac
 	guint response = gtk_dialog_run(GTK_DIALOG(dialog));
 
 	if(response == GTK_RESPONSE_ACCEPT) {
-		gconf_client_set_bool(gconf_client,
-		                      GCONF_KEY_DEVICE_ON,
-		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(device_on_check_button)),
-		                      NULL);
-		gconf_client_set_bool(gconf_client,
-		                      GCONF_KEY_COMMUNICATION_CALL,
-		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(missed_call_check_button)),
-		                      NULL);
-		gconf_client_set_bool(gconf_client,
-		                      GCONF_KEY_COMMUNICATION_SMS,
-		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(text_msg_check_button)),
-		                      NULL);
-		gconf_client_set_bool(gconf_client,
-		                      GCONF_KEY_COMMUNICATION_EMAIL,
-		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(email_msg_check_button)),
-		                      NULL);
-		gconf_client_set_bool(gconf_client,
-		                      GCONF_KEY_COMMUNICATION_IM,
-		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(im_msg_check_button)),
-		                      NULL);
-		gconf_client_set_bool(gconf_client,
-		                      GCONF_KEY_BATTERY_CHARGING,
-		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(charging_check_button)),
-		                      NULL);
-		gconf_client_set_bool(gconf_client,
-		                      GCONF_KEY_COMMON_NOTIFICATION,
-		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(other_check_button)),
-		                      NULL);
+		mce_set_pattern_bool(s_bus,
+		                      KEY_DEVICE_ON,
+		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(device_on_check_button)));
+		mce_set_pattern_bool(s_bus,
+		                      KEY_COMMUNICATION_CALL,
+		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(missed_call_check_button)));
+		mce_set_pattern_bool(s_bus,
+		                      KEY_COMMUNICATION_SMS,
+		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(text_msg_check_button)));
+		mce_set_pattern_bool(s_bus,
+		                      KEY_COMMUNICATION_EMAIL,
+		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(email_msg_check_button)));
+		mce_set_pattern_bool(s_bus,
+		                      KEY_COMMUNICATION_IM,
+		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(im_msg_check_button)));
+		mce_set_pattern_bool(s_bus,
+		                      KEY_BATTERY_CHARGING,
+		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(charging_check_button)));
+		mce_set_pattern_bool(s_bus,
+		                      KEY_COMMON_NOTIFICATION,
+		                      hildon_check_button_get_active(HILDON_CHECK_BUTTON(other_check_button)));
 	}
 	gtk_widget_destroy(dialog);
-	g_object_unref(gconf_client);
+	g_object_unref(s_bus);
 	return OSSO_OK;
 }
 
